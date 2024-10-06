1f7cec9481725a8e9be55fb819fad352c3fdf3cd
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-10-05 19:56:26 -0500

    Updated with tests, started sinks and sources, functional interactive example

--- GoWithTheFlowLab.cpp
+++ GoWithTheFlowLab.cpp
@@ -11,7 +11,7 @@ Lab
 #include "rodModeler.hpp"
 using namespace std;
 
-// Function to move the cursor up by a specified number of lines (ANSI escape code)
+
 void clearConsole() {
     cout << "\033[2J\033[H";  // Clear screen and move the cursor to the top left
 }
@@ -24,20 +24,31 @@ void clearLine() {
 
 int main() {
     //////////////////      TEST AREA    ////////////////////////
-
-    vector <double> testVector = { 1, 2, 3, 4 };
-    HeatFlow hTest;
-    cout << "All checks pass.";
+    HeatFlow hTest(4, 0.05, 100, 0);
+    assert(hTest.getVectorElement(0) == 100);
+    assert(hTest.getVectorSize() == 4);
+    assert(hTest.getK() == 0.05);
+    assert(hTest.getTi() == 100);
+    assert(hTest.getSinks() == 0);
+    cout << "\nUnit tests running:";
+    cout << "\nVerify start point = 100 100 100 100";
+    hTest.test_print();
+    hTest.tick();
+    cout << "\nTick operation performed, verify values = 108.65 100 100 108.65";
+    hTest.test_print();
+    hTest.tick();
+    hTest.tick();
+    hTest.tick();
+    cout << "\n3 tick operation performed, verify values = 129.828 102.344 102.344 129.828";
+    hTest.test_print();
+    cout << "\n\n***All assert checks passed.***";
+    cout << "\n***For user: please verify test values match expectations.***";
+    cout << "\n\n[Program Starting with sample simulation]\n\n";
 
     /////////////////////////////////////////////////////////////
-    
 
 
-
-
-
-
-    /////////// Interactive Area /////////////////////
+    /////////// Interactive Sample Sim Area /////////////////////
 
     HeatFlow rod1;
 
@@ -56,7 +67,7 @@ int main() {
         
         //clearConsole();    // Option that clears the console and display the updated vector and instructions
 
-        cout << "\n== Rod Temperatures ==" << endl;
+        cout << "\n== Rod Temp Sim (in Kelvin) ==" << endl;
         rod1.pretty_print();
         cout << "\nWould you like to apply another tick? (y/n): ";
         getline(cin, userChoice);  // Get the user's input
@@ -73,7 +84,7 @@ int main() {
 
     } while (userChoice != "n" && userChoice != "N");  // Continue until the user presses 'n' or 'N'
 
-    //////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
 
     return 0;
 }
--- rodModeler.hpp
+++ rodModeler.hpp
@@ -1,42 +1,49 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 class HeatFlow {
 private:
     vector<double> rod_;
 
-    int lengthRod_ = 0;
-    double K_ = 0;
+    int lengthRod_ = 0; //Only used when the specific constructor is employed.
+    double K_ = 0.01;
     double Ti_ = 0;
-
-    int numberSourcesSinks = 0;
+    double medium_ = 273;
+    int sources_sinks_ = 0;
 
 public:
 
-    HeatFlow() {};
+    HeatFlow() {}; // A constructor that doesn't need anything.
+
     HeatFlow(int length, double kconstant, double temperature, int sinks) {
+        // This constructor needs specific parameters passed to it.
         setAttributes(length, kconstant, temperature, sinks);
+
+        for (int i = 0; i < lengthRod_; i++) {
+            rod_.push_back(Ti_);
+        }
     };
 
 
     void tick() {
-        
-        vector<double> updatedRod(returnVectorSize());  // Temporary vector to store new values
 
-        for (int i = 0; i < returnVectorSize(); ++i) {
+        vector<double> updatedRod(getVectorSize());  // Temporary vector to store new values
+
+        for (int i = 0; i < getVectorSize(); ++i) {
             if (i == 0) {
-                
-                updatedRod[i] = (293 + getVectorElement(i + 1)) / 2;
+
+                updatedRod[i] = (getVectorElement(i) + K_ * (getVectorElement(i + 1) - 2 * getVectorElement(i) + medium_));
             }
-            else if (i == returnVectorSize() - 1) {
-                
-                updatedRod[i] = (getVectorElement(i - 1) + 293) / 2;
+            else if (i == getVectorSize() - 1) {
+
+                updatedRod[i] = (getVectorElement(i) + K_ * (medium_ - (2 * getVectorElement(i)) + getVectorElement(i - 1)));
             }
             else {
-                
-                updatedRod[i] = (getVectorElement(i - 1) + getVectorElement(i + 1)) / 2;
+
+                updatedRod[i] = (getVectorElement(i) + K_ * (getVectorElement(i + 1) - 2 * getVectorElement(i) + getVectorElement(i - 1)));
             }
         }
 
@@ -48,7 +55,7 @@ public:
         lengthRod_ = length;
         K_ = kconstant;
         Ti_ = temperature;
-        numberSourcesSinks = sinks;
+        sources_sinks_ = sinks;
 
     }
 
@@ -60,11 +67,19 @@ public:
     }
 
     void pretty_print() const {
-        cout << "\n\n";
+        cout << "\n\n| ";
         for (double e : rod_) {
-            cout << "|" << static_cast<int> (e) << "|";
+            //cout << "|" << static_cast<int> (e) << "|";
+            cout << e << " | ";
         }
         cout << "\n\n";
+    }
+
+    void test_print() const {           // For doing start up tests.
+        cout << "\nVector Values: ";
+        for (double e : rod_) {
+            cout << e << " ";
+        }
     }
 
     double getVectorElement(int i) const {
@@ -79,11 +94,74 @@ public:
         rod_[i] = d;
     }
 
-    int returnVectorSize() const {
+    int getVectorSize() const {
         return rod_.size();
     }
 
     int returnRodLength() const {
+        return lengthRod_;
+    }
+
+    void setMediumTemperature(double t) {
+        medium_ = t;
+    }
+
+    double getK() const {
+        return K_;
+    }
+
+    double getTi() const {
+        return Ti_;
+    }
+
+    double getMedium() const {
+        return medium_;
+    }
+
+    int getSinks() const {
+        return sources_sinks_;
+    }
+
+    void applySinksandSources() {  // This function is called in order to set up sinks and sources of heat in the rod.
+
+        vector <int> tempV;
+        double tempSinkSource;
+        int vPositions;
+
+        if (sources_sinks_ == 0)
+        {
+            cout << "No sinks or sources specified.";
+        }
+        else if (rod_.size() - 1 <= sources_sinks_)
+        {
+            sources_sinks_ = rod_.size() - 1;
+            cout << "\nSpecify rod section(s) that has sink or source: ";
+            for (int i = 0; i < sources_sinks_; i++) {
+                cin >> vPositions;
+                while (cin.fail() || vPositions < 0 || vPositions > rod_.size() - 1) {
+                    cin.clear();
+                    cin.ignore();
+                    cout << "\nInvalid input. Please try again: ";
+                    cin >> vPositions;
+                }
+                tempV.push_back(vPositions);
+            }
+
+        }
+        else {
+            cout << "\nSpecify rod section(s) that has sink or source: ";
+            for (int i = 0; i < sources_sinks_; i++) {
+                cin >> vPositions;
+                while (cin.fail() || vPositions < 0 || vPositions > rod_.size() - 1) {
+                    cin.clear();
+                    cin.ignore();
+                    cout << "\nInvalid input. Please try again: ";
+                    cin >> vPositions;
+                }
+                tempV.push_back(vPositions);
+            }
+        }
+
 
     }
 };
